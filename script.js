f1a3a7e91db78ae554da841fc9673b89eddefe30
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2024-04-18 16:19:06 +0530

    elec

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+var gateway = `ws://192.168.199.42/ws`;
+
+var websocket;
+// Init web socket when the page loads
+window.addEventListener('load', onload);
+
+function onload(event) {
+    initWebSocket();
+}
+
+function getReadings() {
+    websocket.send("getReadings");
+}
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection…');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+// When websocket is established, call the getReadings() function
+function onOpen(event) {
+    console.log('Connection opened');
+    getReadings();
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+// Function that receives the message from the ESP32 with the readings
+function onMessage(event) {
+    console.log(event.data);
+    var myObj = JSON.parse(event.data);
+    document.getElementById("message").innerHTML = myObj["message"];
+}
+
+
+
+
