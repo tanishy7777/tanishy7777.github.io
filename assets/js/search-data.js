539d425c763407e50f30f1817a2a3c590004798b
tanishy7777 <143334319+tanishy7777@users.noreply.github.com>
2025-03-17 08:11:09 +0000

    Deploying to gh-pages from @ tanishy7777/tanishy7777.github.io@a7ec02ba08191f11daf306721e9037292e2fa9c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "A few of my github repositories, if you want to have a look at my projects instead see the projects tab on the navbar",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "To view my resume in a pdf format click on the &quot;pdf&quot; button above.",
